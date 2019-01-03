f47f8ff637d1f9229c05a04e3bdf6af4f524e1f7
MathuSum Mut <mathusum.mut@gmail.com>
2019-01-03 16:03:58 +0100

    Updated deploy.js

--- deploy.js
+++ deploy.js
@@ -1,7 +1,9 @@
-loadScript("./build/contracts/Tender.js");
-var TenderAbi = web3.eth.contract(TenderBuild.abi);
-var Tender = TenderAbi.new({from:eth.coinbase, data: TenderAbi.bytecode, gas: 1000000});
-
+//Loading TenderApi contract
 loadScript("./build/contracts/TenderApi.js");
 var TenderApiAbi = web3.eth.contract(TenderApiBuild.abi);
-var TenderApi = TenderAbi.new({from:eth.coinbase, data: TenderApiAbi.bytecode, gas: 1000000});
+var TenderApi = TenderApiAbi.new({from: eth.coinbase, data: TenderApiAbi.bytecode, gas: 1000000});
+
+//Loading Tender contract (requires constructor values)
+loadScript("./build/contracts/Tender.js");
+var TenderAbi = web3.eth.contract(TenderBuild.abi);
+var Tender = TenderAbi.new({from: eth.coinbase, data: TenderAbi.bytecode, gas: 1000000});
