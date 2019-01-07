a76eb27c0454fa0669b1f0e651a43915d4dcd110
MathuSum Mut <mathusum.mut@gmail.com>
2019-01-07 16:50:58 +0100

    Added test file

---
+++ test/TenderInterface.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24;
+
+//Version 0.01
+//This interface serves to expose and document main contract functionality
+//This is meant to be set in stone as much as possible after completion
+interface TenderInterface {
+	//Increments value
+	function incrementValue() external returns (uint8);
+}
