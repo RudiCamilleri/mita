4d234494540d3a2b9a37fc8edce8e1c2bffc7ce8
Matthew Sammut <matthew.d.sammut@gov.mt>
2019-02-11 14:33:53 +0100

    Added solidity 0.5 support

--- contracts/Tender.sol
+++ contracts/Tender.sol
@@ -6,14 +6,13 @@ import "./TenderInterface.sol";
 //This is the contract functionality implementation
 //This contract is upgradeable as long as it keeps the same TenderInterface implementation
 contract Tender is TenderInterface {
-	address public owner; //the TenderApi smart contract instance
+	address payable public owner; //the TenderApi smart contract instance
 
 	//Initializes the smart contract
-	constructor(address apiAddress,
+	constructor(address payable apiAddress,
 		uint256 smallServerPrice, uint256 mediumServerPrice, uint256 largeServerPrice,
 		uint16 min, uint16 max, uint16 daysForDelivery, uint256 penaltyPerDay, uint256 penaltyCap,
-		uint256 maximumCostofExtras, uint64 expiryDate, bytes8 operatorId, uint256 guaranteeRequired,
-		uint32 description) public {
+		uint256 maximumCostofExtras, uint64 expiryDate, bytes8 operatorId, uint256 guaranteeRequired) public {
 		owner = apiAddress;
 	}
 
@@ -28,7 +27,11 @@ contract Tender is TenderInterface {
 	//==    ALL FUNCTIONS MUST BE MARKED AS RESTRICTED!    ==
 	//=======================================================
 
-	function incrementValue() public restricted returns (uint8) {
-		return ++value;
+	function endContract() public restricted {
+		selfdestruct(owner);
 	}
+
+	/*function incrementValue() public restricted returns (uint8) {
+		return ++value;
+	}*/
 }
--- contracts/TenderApi.sol
+++ contracts/TenderApi.sol
@@ -7,7 +7,7 @@ import "./TenderInterface.sol";
 //This is meant to be set in stone as much as possible after completion
 contract TenderApi is TenderInterface {
 	TenderInterface public current; //the current contract implementation
-	address public owner; //the creator of the smart contract
+	address payable public owner; //the creator of the smart contract
 
 	//Initializes the smart contract
 	constructor() public {
@@ -25,70 +25,67 @@ contract TenderApi is TenderInterface {
 	//======================================================
 
 	//Ends the contract
-	function endContract() external{
-		return current.endContract();
+	function endContract() external {
+		current.endContract();
+		selfdestruct(owner);
 	}
+
 	//passes refundable deposit.
-	function topUpPerformanceGuarantee() external{
+	function topUpPerformanceGuarantee() external {
 		return current.topUpPerformanceGuarantee();
 	}
 
 	//stops the contract if payment isnt made.
-	function stopOrder(uint32 orderNumber, uint8 penalty) external{
+	function stopOrder(uint32 orderNumber, uint8 penalty) external {
 		//uint32 - Order number, uint8 - Penalty to be taken
 		return current.stopOrder(orderNumber, penalty);
 	}
 
     //if order was delivered proceed
-	function deliveryAcceptance(uint32 orderNumber, uint16 serverAmount) external{
+	function deliveryAcceptance(uint32 orderNumber, uint16 serverAmount) external {
 		//uint32 - Order number, unit16 server amount as a whole
 		return current.deliveryAcceptance(orderNumber, serverAmount);
 	}
 
 	//to confirm order is delivered
-	function markDelivered(uint32 orderNumber) external{
+	function markDelivered(uint32 orderNumber) external {
 		//uint32 - Order number
 		return current.markDelivered(orderNumber);
 	}
 
 	//automatically accepts extension
-	function defaultAcceptanceOfOrderDeadlineExtension(uint32 orderNumber) external{
+	function defaultAcceptanceOfOrderDeadlineExtension(uint32 orderNumber) external {
 		//uint32 - Order number
 		return current.defaultAcceptanceOfOrderDeadlineExtension(orderNumber);
 	}
 
 	//to reject the extension
-	function rejectOrderDeadlineExtension(uint32 orderNumber) external{
+	function rejectOrderDeadlineExtension(uint32 orderNumber) external {
 		//uint32 - Order number
 		return current.rejectOrderDeadlineExtension(orderNumber);
 	}
 
 	//accepts deadline manually
-	function acceptOrderDeadlineExtension(uint32 orderNumber, uint64 dateExtension) external{
+	function acceptOrderDeadlineExtension(uint32 orderNumber, uint64 dateExtension) external {
 		//uint32 - Order number, uint 64 is checking date of extension.
 		return current.acceptOrderDeadlineExtension(orderNumber, dateExtension);
 	}
 
 	//requests an extension of a deadline
-	function requestOrderDeadlineExtension(uint32 orderNumber,bytes32 reason, uint64 dateExtension ) external{
+	function requestOrderDeadlineExtension(uint32 orderNumber,bytes32 reason, uint64 dateExtension ) external {
 		//bytes 32 to store explanation //uint 64 is checking date of extension.
 		return current.requestOrderDeadlineExtension(orderNumber, reason, dateExtension);
 	}
 
 	//cancels order
-	function cancelOrder(uint32 orderNumber) external{
+	function cancelOrder(uint32 orderNumber) external {
 		//uint32 - Order number
 		return current.cancelOrder(orderNumber);
 	}
 
 	//creates order
-	function createOrder(uint32 orderNumber, uint16 serverAmount, bytes32 costDescription) external{
+	function createOrder(uint32 orderNumber, uint16 serverAmount, bytes32 costDescription) external {
 		//uint32 - Order number, uint16 server amoutn as a whole , bytes32 description of total price.
 		return current.createOrder(orderNumber, serverAmount, costDescription);
-	}
-
-	//accepts contract
-	function acceptContract() external{
-		return current.acceptContract();
 	}
 }
