67ed8f27f47fcd201f34b8e366363c14d282b0a9
MathuSum Mut <mathusum.mut@gmail.com>
2019-03-04 09:21:18 +0100

    Updated scripts

---
+++ run-remix+nethereum+vscode.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Starting Visual Studio Code...
+cmd /c code "%cd%\contracts"
+.\run-remix+nethereum.bat
--- run-remix.bat
+++ run-remix.bat
@@ -1,19 +1,8 @@
 @echo off
-cd contracts
-title Compiling smart contracts...
-echo Compiling smart contracts... (using truffle)
-call truffle compile
-cd ..
-echo Minifying Output...
-call .\minify-json.exe build/contracts build/contracts
+title Running Remixd...
 start /b call .\start-ganache-cli.bat <NUL >ganache-output.txt
 cd contracts
-title Running Remixd...
 echo Opening URL: http://remix.ethereum.org/#optimize=true^&version=soljson-v0.5.0+commit.1d4f565a.js
 start "" "http://remix.ethereum.org/#optimize=true&version=soljson-v0.5.0+commit.1d4f565a.js"
 echo Starting Remixd...
-start /b cmd /c remixd -s %cd% --remix-ide http://remix.ethereum.org <NUL
-echo Launching REPL...
-title Nethereum C# Console
-rem call dotnet-script -i "%cd%\..\init-csharp-repl.csx"
-call scriptcs -script "%cd%\..\init-csharp-repl.csx" -Repl
+call remixd -s %cd% --remix-ide http://remix.ethereum.org <NUL
