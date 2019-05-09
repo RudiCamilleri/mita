0aaa51eb1d1d9c58ef7cbbdbadeda486bcb81573
mathusummut <mathusum.mut@gmail.com>
2019-05-09 21:11:38 +0200

    Added build directory configuration

--- truffle-config.js
+++ truffle-config.js
@@ -10,7 +10,8 @@ module.exports = {
 			port: 8545, //default ganache-cli port
 			network_id: "*", //match any network id
 			gas: 2573636023, //gas limit used for deploys
-			gasPrice: 2000000000 //Gas price used for deploys (default: 100 Shannon = 100000000000)
+			gasPrice: 2000000000, //Gas price used for deploys (default: 100 Shannon = 100000000000)
+			contracts_build_directory: "./build"
 		}
 	},
 	compilers: {
